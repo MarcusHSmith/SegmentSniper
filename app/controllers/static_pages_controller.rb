0b42ae020a07db46a57b9774756b125504312886
Marcus Smith <mail4marcus@comcast.net>
2014-06-12 23:02:54 -0700

    working Strava API calls

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
-  	p "HOME"
 	@client = Strava::Api::V3::Client.new(:access_token => "3db13d4c374cab1f8f2c225eb3eaa683cc0c7f89")
-	p @client
-	p @client.retrieve_current_athlete
+	@segment = @client.retrieve_a_segment(997729)
+	@leaderboard = @client.segment_leaderboards(997729)
+	@athlete_id = @leaderboard["entries"][1]["athlete_id"]
+	@athlete = @client.retrieve_another_athlete(@athlete_id)
+	puts JSON.pretty_generate(@athlete)
   end
   def about
   end
